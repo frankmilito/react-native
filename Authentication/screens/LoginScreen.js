59ff5a30c3c2d3b2e346eb3889183f9a0ae9cddb
frankmilito <frankobidike20@gmail.com>
2023-10-29 14:28:44 +0100

    added expo splash screen

--- Authentication/screens/LoginScreen.js
+++ Authentication/screens/LoginScreen.js
@@ -1,7 +1,25 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useContext, useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { login } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { AuthContext } from "../store/authContext";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const authCtx = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoginScreen, setIsLoginScreen] = useState(true);
+  const signinHandler = async (data) => {
+    setIsLoading(true);
+    const token = await login(data);
+
+    authCtx.authenticate(token);
+    setIsLoading(false);
+  };
+
+  if (isLoading) {
+    return <LoadingOverlay message={"Logging in..."} />;
+  }
+  return <AuthContent isLogin onAuthenticate={signinHandler} />;
 }
 
 export default LoginScreen;
--- Authentication/screens/SignupScreen.js
+++ Authentication/screens/SignupScreen.js
@@ -1,7 +1,23 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useContext, useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { AuthContext } from "../store/authContext";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const authCtx = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const signupHandler = async (data) => {
+    setIsLoading(true);
+    const token = await createUser(data);
+    authCtx.authenticate(token);
+    setIsLoading(false);
+  };
+
+  if (isLoading) {
+    return <LoadingOverlay message={"Creating user..."} />;
+  }
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
