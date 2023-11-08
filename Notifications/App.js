6f41ac9f78bb49d54c9095a6d1d867908d2d018c
frankmilito <frankobidike20@gmail.com>
2023-11-08 15:09:22 +0100

    fixed notifications issues for ios

--- Notifications/App.js
+++ Notifications/App.js
@@ -12,18 +12,72 @@ Notifications.setNotificationHandler({
     };
   },
 });
+
+async function requestNotificationPermissions() {
+  const { status } = await Notifications.getPermissionsAsync();
+  if (status !== "granted") {
+    const { status: newStatus } = await Notifications.requestPermissionsAsync();
+    if (newStatus === "granted") {
+      return true; // Permission granted
+    } else {
+      return false; // Permission denied
+    }
+  }
+  return true; // Permission was already granted
+}
+
 export default function App() {
-  const scheduleNotification = () => {
-    Notifications.scheduleNotificationAsync({
-      content: {
-        title: "My first notification",
-        body: "This is the detail of this notification from mili",
-        data: { user: "mili is going places" },
-      },
-      trigger: {
-        seconds: 5,
-      },
-    });
+  useEffect(() => {
+    async function configurePushNotifification() {
+      const hasPermissions = await requestNotificationPermissions();
+
+      if (hasPermissions) {
+        try {
+          const pushTokenData = await Notifications.getExpoPushTokenAsync();
+          console.log(pushTokenData);
+        } catch (error) {
+          console.log(error, "error");
+        }
+      }
+    }
+    configurePushNotifification();
+  }, []);
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        // console.log(notification.request.content.data);
+        console.log("notification");
+      }
+    );
+
+    const response = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log(response);
+        console.log("response");
+      }
+    );
+    return () => {
+      response.remove();
+      subscription.remove();
+    };
+  }, []);
+  const scheduleNotification = async () => {
+    const hasPushNotificationPermissionGranted =
+      await requestNotificationPermissions();
+
+    if (hasPushNotificationPermissionGranted) {
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: "My first notification",
+          body: "This is the detail of this notification from mili",
+          data: { user: "mili is going places" },
+        },
+        trigger: {
+          seconds: 3,
+        },
+      });
+    }
   };
   return (
     <View style={styles.container}>
