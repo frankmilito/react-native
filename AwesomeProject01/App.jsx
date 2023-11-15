40313ad4c5fd7ffa6a7f671eaa128b0e6aee5431
frankmilito <frankobidike20@gmail.com>
2023-11-15 07:06:22 +0100

    elevated cards

--- AwesomeProject01/App.jsx
+++ AwesomeProject01/App.jsx
@@ -1,17 +1,17 @@
 import {SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
 import FlatCard from './components/FlatCard';
+import ElevatedCard from './components/ElevatedCard';
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
         <FlatCard />
+        <ElevatedCard />
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
